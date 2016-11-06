aa0450706eba26481378b1160972e9c545608d2e
Administrateur <Administrateur@HP88456513133>
2016-11-06 19:34:00 +0100

    creation du memento source c

---
+++ memento.c
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   memento.c
+ * Author: Administrateur
+ * 
+ * Created on 6 novembre 2016, 19:06
+ */
+
+#include "memento.h"
