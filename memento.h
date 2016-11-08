f4efb1d93f070394dce162947ce0b9b6349a0b99
jean@antix1 <jean@antix1>
2016-11-08 17:22:30 +0100

    écriture struct et prototype fonctions memento.h

--- memento.h
+++ memento.h
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   memento.h
  * Author: Administrateur
  *
@@ -13,17 +13,33 @@
 
 #ifndef MEMENTO_H
 #define MEMENTO_H
+
+#define NB_UNDO 5	//nombre max de undo géré
+
 typedef struct s_mvt {
     int row ;
     int column ;
 }mvt;
+
 typedef struct s_memento{
-    int idRollback ;        //identifiant du mouvement
-    mvt mvtStart ;          //coord depart du mouvement
-    mvt mvtEnd ;            //coord arrivée du mouvement
+    int idRollback ;        // identifiant du mouvement
+    mvt mvtStart ;          // coord depart du mouvement
+    mvt mvtEnd ;            // coord arrivée du mouvement
  } memento ;
+
 typedef memento* pMemento ;
-void mementoNew(mvt , mvt) ;
-pMemento mementoGetSaveState() ;
+
+typedef pMemento mementoArrayList[NB_UNDO] ;
+
+void mementoNew(mvt , mvt) ; 		// ecrit la structure
+pMemento mementoGetSaveState() ;	// calcul les mvt precedent
+
+pMemento originatorSaveToMemento() ; 		// appel mementoNew qui ecrit la struct
+void originatorRestoreFromMemento(pMemento) // appel mementoGetSaveState à l'indice caretakerGetMemento(int)
+											// et modifie la matrice
+
+void caretakerAddMemento(pMemento) ; // ajoute un pointeur dans mementoArrayList
+pMemento caretakerGetMemento(int) ;  // retourne pointeur memento à l'indice donné
+									 // faudra gérer la Liste en detruisant juste après le numero UNDO?
 
 #endif /* MEMENTO_H */
