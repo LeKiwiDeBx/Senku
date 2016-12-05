ba67f7a08172808eac84bb678915ab511baed950
Administrateur <Administrateur@HP88456513133>
2016-12-05 14:56:52 +0100

    draft gtk dialog menu choice shapes

--- main.c
+++ main.c
@@ -22,7 +22,7 @@
  *       __         __ ___ _      ___
  *      / /  ___   / //_(_) | /| / (_)
  *     / /__/ -_) / ,< / /| |/ |/ / /
- *    /____/\__/ /_/|_/_/ |__/|__/_/   présente
+ *    /____/\__/ /_/|_/_/ |__/|__/_/   [°} présente
  *
  *       _____            __
  *      / ___/___  ____  / /____  __
@@ -38,6 +38,7 @@
 #include <glib.h>
 #include <glib/gprintf.h>
 #include "board.h"
+
 /*
   AND THE SHOW...
  */
@@ -50,13 +51,13 @@ main( int argc, char *argv[] ) {
     GtkWidget *lbTitle1, *lbTitle2, *lbl4 ;
     GtkWidget *pVbox ;
     gtk_init( &argc, &argv ) ;
-    
+
     //  Creation window main
     window = gtk_window_new( GTK_WINDOW_TOPLEVEL ) ;
     gtk_window_set_position( GTK_WINDOW( window ), GTK_WIN_POS_CENTER ) ;
     gtk_window_set_title( GTK_WINDOW( window ), "Senku GTK Beta 2.0" ) ;
     gtk_window_set_default_size( GTK_WINDOW( window ), 360, 340 ) ;
-    gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+    gtk_container_set_border_width( GTK_CONTAINER( window ), 10 ) ;
     GtkWidget *pGridMain = gtk_grid_new( ) ;
     gtk_container_add( GTK_CONTAINER( window ), pGridMain ) ;
 
@@ -72,60 +73,79 @@ main( int argc, char *argv[] ) {
             gtk_grid_attach( GTK_GRID( grid ), lbl4, i, k, 1, 1 ) ;
         }
     }
-    
+
     //    Menu droit
     pVbox = gtk_vbox_new( TRUE, 0 ) ;
-    gtk_box_set_homogeneous(GTK_BOX(pVbox) , FALSE) ;
-    gtk_box_set_spacing(GTK_BOX(pVbox), 10) ;
-    gtk_widget_set_margin_left(GTK_WIDGET(pVbox), 20) ;
-    
-    GtkWidget *plbTime = gtk_label_new( "Time:" ) ; 
+    gtk_box_set_homogeneous( GTK_BOX( pVbox ), FALSE ) ;
+    gtk_box_set_spacing( GTK_BOX( pVbox ), 10 ) ;
+    gtk_widget_set_margin_left( GTK_WIDGET( pVbox ), 20 ) ;
+
+    GtkWidget *plbTime = gtk_label_new( "Time:" ) ;
     GtkWidget *plbTimeValue = gtk_label_new( " 0 s" ) ;
     gtk_box_pack_start( GTK_BOX( pVbox ), plbTime, FALSE, FALSE, 10 ) ;
     gtk_box_pack_start( GTK_BOX( pVbox ), plbTimeValue, FALSE, FALSE, 0 ) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbTime), GTK_ALIGN_START) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbTimeValue), GTK_ALIGN_START) ;
-    
+    gtk_widget_set_halign( GTK_WIDGET( plbTime ), GTK_ALIGN_START ) ;
+    gtk_widget_set_halign( GTK_WIDGET( plbTimeValue ), GTK_ALIGN_START ) ;
+
     GtkWidget *plbPegs = gtk_label_new( "Pegs remaining:" ) ;
     GtkWidget *plbPegsValue = gtk_label_new( " 0 " ) ;
     gtk_box_pack_start( GTK_BOX( pVbox ), plbPegs, FALSE, FALSE, 0 ) ;
     gtk_box_pack_start( GTK_BOX( pVbox ), plbPegsValue, FALSE, FALSE, 0 ) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbPegs), GTK_ALIGN_START) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbPegsValue), GTK_ALIGN_START) ;
+    gtk_widget_set_halign( GTK_WIDGET( plbPegs ), GTK_ALIGN_START ) ;
+    gtk_widget_set_halign( GTK_WIDGET( plbPegsValue ), GTK_ALIGN_START ) ;
 
     GtkWidget *plbBonus = gtk_label_new( "Bonus:" ) ;
     GtkWidget *plbBonusValue = gtk_label_new( " 0 " ) ;
     gtk_box_pack_start( GTK_BOX( pVbox ), plbBonus, FALSE, FALSE, 0 ) ;
     gtk_box_pack_start( GTK_BOX( pVbox ), plbBonusValue, FALSE, FALSE, 0 ) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbBonus), GTK_ALIGN_START) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbBonusValue), GTK_ALIGN_START) ;
-
+    gtk_widget_set_halign( GTK_WIDGET( plbBonus ), GTK_ALIGN_START ) ;
+    gtk_widget_set_halign( GTK_WIDGET( plbBonusValue ), GTK_ALIGN_START ) ;
     gtk_container_add( GTK_CONTAINER( pGridMain ), pVbox ) ;
 
-    // label comments game in progress
+    /** 
+     * label comments game in progress 
+     */
     gtk_orientable_set_orientation( GTK_ORIENTABLE( pGridMain ), GTK_ORIENTATION_VERTICAL ) ;
-    GtkWidget *pfrComments = gtk_frame_new(NULL) ;
-    gtk_frame_set_label(GTK_FRAME(pfrComments), "Comments: ") ;
-    GtkWidget *plbComments = gtk_label_new("Comments in progress...") ;
-    gtk_container_add(GTK_CONTAINER(pfrComments), plbComments) ;
-    gtk_widget_set_margin_top(GTK_WIDGET(pfrComments), 20) ;
-    gtk_widget_set_halign(GTK_WIDGET(plbComments), GTK_ALIGN_START) ;
-    gtk_grid_attach( GTK_GRID( pGridMain ), pfrComments, 0,1,2,1 ) ;
+    GtkWidget *pfrComments = gtk_frame_new( NULL ) ;
+    gtk_frame_set_label( GTK_FRAME( pfrComments ), "Comments: " ) ;
+    GtkWidget *plbComments = gtk_label_new( "Comments in progress..." ) ;
+    gtk_container_add( GTK_CONTAINER( pfrComments ), plbComments ) ;
+    gtk_widget_set_margin_top( GTK_WIDGET( pfrComments ), 20 ) ;
+    gtk_widget_set_halign( GTK_WIDGET( plbComments ), GTK_ALIGN_START ) ;
+    gtk_grid_attach( GTK_GRID( pGridMain ), pfrComments, 0, 1, 2, 1 ) ;
 
-    // Bouton bas    
+    // Button bottom 
     GtkWidget *pHbox = gtk_hbox_new( TRUE, 0 ) ;
-    gtk_widget_set_margin_top(GTK_WIDGET(pHbox), 20) ;
-    gtk_widget_set_halign(GTK_WIDGET(pHbox), GTK_ALIGN_END) ;
+    gtk_widget_set_margin_top( GTK_WIDGET( pHbox ), 20 ) ;
+    gtk_widget_set_halign( GTK_WIDGET( pHbox ), GTK_ALIGN_END ) ;
     GtkWidget *pButtonUndo = gtk_button_new_with_label( "Undo" ) ;
     gtk_box_pack_start( GTK_BOX( pHbox ), pButtonUndo, FALSE, FALSE, 15 ) ;
     GtkWidget *pButtonQuit = gtk_button_new_with_label( "Quit" ) ;
-    
-    gtk_box_pack_end( GTK_BOX( pHbox ), pButtonQuit, TRUE, TRUE, 15 ) ;
-    gtk_grid_attach( GTK_GRID( pGridMain ), pHbox,1,2,1,1 ) ;
 
+    gtk_box_pack_end( GTK_BOX( pHbox ), pButtonQuit, TRUE, TRUE, 15 ) ;
+    gtk_grid_attach( GTK_GRID( pGridMain ), pHbox, 1, 2, 1, 1 ) ;
     g_signal_connect( G_OBJECT( window ), "destroy", G_CALLBACK( OnDestroy ), NULL ) ;
     g_signal_connect( G_OBJECT( pButtonQuit ), "clicked", G_CALLBACK( OnDestroy ), NULL ) ;
+            
+    /**
+     * Dialog box Menu 
+     * 
+     */
+   GtkWidget *pBoxMenu = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+   gtk_window_set_title(GTK_WINDOW(pBoxMenu), "Shapes choice") ;
+   gtk_window_set_modal(GTK_WINDOW(pBoxMenu), TRUE) ;
+   //
+    GtkWidget *pBoxMenuOption = gtk_box_new (GTK_ORIENTATION_VERTICAL, 2);
+    gtk_box_set_homogeneous (GTK_BOX (pBoxMenuOption), TRUE);
+    GtkWidget *radio1 = gtk_radio_button_new_with_label (NULL ,"label 1");
+    GtkWidget *radio2 = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON (radio1),
+                                                         "label 2");
+    gtk_box_pack_start (GTK_BOX (pBoxMenuOption), radio1, TRUE, TRUE, 2);
+    gtk_box_pack_start (GTK_BOX (pBoxMenuOption), radio2, TRUE, TRUE, 2);
+    
+    gtk_container_add(GTK_CONTAINER(pBoxMenu), pBoxMenuOption) ;
     gtk_widget_show_all( window ) ;
+    gtk_widget_show_all(GTK_DIALOG(pBoxMenu)) ;
     gtk_main( ) ;
     EXIT_SUCCESS ;
     /*
@@ -137,6 +157,7 @@ main( int argc, char *argv[] ) {
         return 0;
         }
      */
+    
 }
 
 /**
